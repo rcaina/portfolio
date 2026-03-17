2bf69a649fc895768a6efe779a8677ac9ab70d41
Renzo <renzocainaedge@gmail.com>
2026-03-17 15:52:39 -0600

    add graph tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,20 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/lny5tsf.css" />
+        <meta property="og:title" content="Portfolio" />
+        <meta
+          property="og:description"
+          content="Personal portfolio built using Next.js and deployed with Vercel."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/icons/apple-icon.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Portfolio" />
+        <meta
+          name="twitter:description"
+          content="Personal portfolio built using Next.js and deployed with Vercel."
+        />
+        <meta name="twitter:image" content="/icons/apple-icon.png" />
       </Head>
       <body>
         <Main />
