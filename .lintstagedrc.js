25f1ecfd23f6787d364e0c21857eb9434b476dc8
Renzo <renzocainaedge@gmail.com>
2026-05-25 07:28:36 -0600

    update commit calls, remove unneeded checks

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,16 +1,7 @@
 module.exports = {
-  // Type check TypeScript files
-  "**/*.(ts|tsx)": () => "npx tsc --noEmit",
-
-  // Lint & Prettify TS and JS files
-  "**/*.(ts|tsx|js)": (filenames) => [
-    `npm run prettier --write ${filenames.join(" ")}`,
-    `npx eslint --max-warnings=0 ${filenames.join(" ")}`,
+  "**/*.{ts,tsx,js}": (files) => [
+    `npx prettier --write ${files.join(" ")}`,
+    `npx eslint --max-warnings=0 ${files.join(" ")}`,
   ],
-
-  // Prettify only Markdown and JSON files
-  "**/*.(md|json)": (filenames) =>
-    `npm run prettier --write ${filenames.join(" ")}`,
-
-  "**/*.(md|json)": () => "npm run prettier --write",
+  "**/*.{md,json}": (files) => `npx prettier --write ${files.join(" ")}`,
 };
